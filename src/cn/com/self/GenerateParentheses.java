ee5105f287e63336ec563670dea267243ab1948a
newbone <1106690435@qq.com>
2019-05-12 13:50:26 +0800

    new problem

---
+++ src/cn/com/self/GenerateParentheses.java
@@ -0,0 +1,45 @@
+package cn.com.self;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ *
+ * For example, given n = 3, a solution set is:
+ *
+ * [
+ *   "((()))",
+ *   "(()())",
+ *   "(())()",
+ *   "()(())",
+ *   "()()()"
+ * ]
+ *
+ * 2049.5.12 zx
+ * 递归
+ */
+public class GenerateParentheses {
+    public static void main(String[] args) {
+        GenerateParentheses tmp = new GenerateParentheses();
+        int n = 3;
+        System.out.println(tmp.generateParenthesis(n).toString());
+    }
+
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+    public void backtrack(List<String> list, String str, int open, int close, int max){
+        if(str.length() == max*2){
+            list.add(str);
+            return;
+        }
+
+        if(open < max)
+            backtrack(list, str+"(", open+1, close, max);
+        if(close < open)
+            backtrack(list, str+")", open, close+1, max);
+    }
+}
