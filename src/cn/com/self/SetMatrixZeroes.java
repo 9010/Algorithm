6fbfdd4142f8fd38f89d9d5c3b60677a3977edd8
Prime <1106690435@qq.com>
2019-06-25 17:45:29 +0800

    new problem

--- src/cn/com/self/SetMatrixZeroes.java
+++ src/cn/com/self/SetMatrixZeroes.java
@@ -13,7 +13,14 @@ import java.util.Set;
  */
 public class SetMatrixZeroes {
     public static void main(String[] args) {
-        int[][] matrix = {{0,1,2,0}, {3,4,5,2}, {1,3,1,5}};
+        //int[][] matrix = {{0,1,2,0}, {3,4,5,2}, {1,3,1,5}};
+        int[][] matrix = {{1}, {0}};
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
         SetMatrixZeroes tmp = new SetMatrixZeroes();
         tmp.setZeroes(matrix);
         for(int i = 0; i < matrix.length; i++){
@@ -25,20 +32,22 @@ public class SetMatrixZeroes {
     }
     public void setZeroes(int[][] matrix) {
         if(matrix.length == 0) return;
-        Set<Integer> line = new HashSet<>();
-        Set<Integer> list = new HashSet<>();
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[] line = new int[row];
+        int[] list = new int[col];
 
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[0].length; j++){
                 if(matrix[i][j] == 0){
-                    line.add(i);
-                    list.add(j);
+                    line[i] = 1;
+                    list[j] = 1;
                 }
             }
         }
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[0].length; j++){
-                if(line.contains(i) || list.contains(j)){
+                if(line[i] == 1 || list[j] == 1){
                     matrix[i][j] = 0;
                 }
             }
