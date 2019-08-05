b4395effd760eda67ea3da4dbb5bb2436ffd0baf
newbone <1106690435@qq.com>
2019-08-05 16:53:09 +0800

    TreeNode define

---
+++ src/cn/com/self/BinaryTreeInorderTraversal.java
@@ -0,0 +1,4 @@
+package cn.com.self;
+
+public class BinaryTreeInorderTraversal {
+}
---
+++ src/cn/com/self/TreeNode.java
@@ -0,0 +1,11 @@
+package cn.com.self;
+
+/**
+ * 通用二叉树结点类
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
