9f064be5ed085e0c244bbef87df8ad597797ea96
Prime <1106690435@qq.com>
2019-09-11 14:39:14 +0800

    新通用节点类

--- src/cn/com/self/Node.java
+++ src/cn/com/self/Node.java
@@ -1,20 +1,15 @@
 package cn.com.self;
 
-/**
- * 通用Node节点类
- */
-public class Node {
+import java.util.List;
+
+class Node {
     public int val;
-    public Node left;
-    public Node right;
-    public Node next;
+    public List<Node> neighbors;
 
     public Node() {}
 
-    public Node(int _val,Node _left,Node _right,Node _next) {
+    public Node(int _val,List<Node> _neighbors) {
         val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
+        neighbors = _neighbors;
     }
-}
+};
